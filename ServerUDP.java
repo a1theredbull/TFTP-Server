ad3b8c442f7f26fec3a058445e1f3d79c1cb3473
Chau <chaua@arch04.cis.gvsu.edu>
2012-10-26 11:56:37 -0400

    Working on ServerUDP

--- ServerUDP.java
+++ ServerUDP.java
@@ -3,8 +3,10 @@ import java.net.*;
 
 class ServerUDP
 {
-	private static final int MAX_BYTES = 1024;
-	private static final int PORT = 9800;
+	private static final int MAX_BYTES = 512;
+	private static final int PORT = 69;
+
+	private File readDirectory, writeDirectory;
 
 	public static void main(String args[]) throws Exception
 	{
@@ -12,12 +14,21 @@ class ServerUDP
 		while(true)
 		{
 			byte[] receiveData = new byte[MAX_BYTES];
-			DatagramPacket receivePacket = new DatagramPacket(
-				receiveData, receiveData.length);
-			serverSocket.receive(receivePacket);
-			String msg = new String(receivePacket.getData());
-			String reply = msg.toUpperCase();
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			try
+			{
+				serverSocket.receive(receivePacket);
+				Runnable r = new ClientHandler(receivePacket, receiveData);
+				Thread t = new Thread(r);
+				t.start();
+			}
+			catch(IOException ex)
+			{
+				System.err.println("Failed to receive.\n");
+				System.exit(1);
+			}
 			
+			/*
 			byte[] sendData = new byte[MAX_BYTES];
 			sendData = reply.getBytes();
 			InetAddress IPAddress = receivePacket.getAddress();
@@ -25,6 +36,36 @@ class ServerUDP
 			DatagramPacket sendPacket = new DatagramPacket(
 				sendData, sendData.length, IPAddress, port);
 			serverSocket.send(sendPacket);
+			*/
 		}
+	}
+}
+
+class ClientHandler implements Runnable
+{
+	private static final int MAX_BYTES = 512;
+
+	byte[] receiveData;
+	DatagramPacket receivePacket;
+	byte[] opcode = new byte[2];
+
+	ClientHandler(DatagramPacket receivePacket, byte[] receiveData)
+	{
+		this.receivePacket = receivePacket;
+		this.receiveData = receiveData;
+	}
+
+	public void run()
+	{
+		decodeBytes(receiveData);
+	}
+
+	public boolean decodeBytes(byte[] toDecode)
+	{
+		String str = new String(toDecode);
+		System.out.println(str);
+		opcode[0] = toDecode[0];
+		opcode[1] = toDecode[1];
+		return true;
 	}
 }
