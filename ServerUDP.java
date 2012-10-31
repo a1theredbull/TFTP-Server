5d5809045ad097c550a26f5302560bcd291f12c3
Alexander Chau <chaua@mail.gvsu.edu>
2012-10-31 00:24:12 -0400

    Dynamic block allocation and sending added

--- ServerUDP.java
+++ ServerUDP.java
@@ -25,16 +25,6 @@ class ServerUDP
 				System.err.println("Failed to receive.\n");
 				System.exit(1);
 			}
-			
-			/*
-			byte[] sendData = new byte[MAX_BYTES];
-			sendData = reply.getBytes();
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			DatagramPacket sendPacket = new DatagramPacket(
-				sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
-			*/
 		}
 	}
 }
@@ -51,14 +41,20 @@ class RequestHandler implements Runnable
 	private String readDirectory, writeDirectory;
 	private DatagramSocket serverSocket;
 	
+	private InetAddress IPAddress;
+	private int port;
+	
 	RequestHandler(DatagramPacket receivePacket, byte[] packetBytes, DatagramSocket serverSocket)
 	{
 		this.receivePacket = receivePacket;
 		this.packetBytes = packetBytes;
 		this.serverSocket = serverSocket;
 		
-		readDirectory = "C:/Users/Alex/Desktop/"; //change directory here
+		readDirectory = "C:/Users/Alex/Desktop/Test/"; //change directory here
 		writeDirectory = "C:/Users/Alex/Desktop/"; //change directory here
+		
+		IPAddress = receivePacket.getAddress();
+		port = receivePacket.getPort();
 	}
 
 	public void run()
@@ -104,61 +100,40 @@ class RequestHandler implements Runnable
 			RandomAccessFile f = new RandomAccessFile(fileToSend, "rw");
 			DatagramPacket sendPacket;
 			
-			byte[] all_data = new byte[(int)f.length()];
-			f.read(all_data);
-			int remaining_data_length = all_data.length;
-			System.out.println("LENGTH: " + all_data.length);
+			byte[] fileData = new byte[(int)f.length()];
+			f.read(fileData);
+			f.close();
 			
-			byte[] sendData = new byte[all_data.length+4]; //BLOCK_SIZE
-			sendData[0] = '0';
-			sendData[1] = '1';
-			sendData[2] = '0';
-			sendData[3] = '0';
+			int remainingLength = fileData.length;
+			System.out.println("LENGTH: " + remainingLength);
+			int packetSize = BLOCK_SIZE;
+			int blockNum = 1;
+			int fileDataOffset = 0;
 			
-			for(int i = 4; i < all_data.length+4; i++)
+			do
 			{
-				sendData[i] = all_data[i-4];
+				if(remainingLength < BLOCK_SIZE) //last packet sent
+					packetSize = remainingLength;
+				
+				byte[] sendBytes = new byte[packetSize+4];
+				sendBytes[0] = 0;
+				sendBytes[1] = 3;
+				sendBytes[2] = 0;
+				sendBytes[3] = (byte)blockNum;
+				
+				for(int i = 0; i < packetSize; i++)
+					sendBytes[i+4] = fileData[i + fileDataOffset];
+				
+				sendPacket = new DatagramPacket(
+						sendBytes, sendBytes.length, IPAddress, port);
+				
+				serverSocket.send(sendPacket);
+				
+				remainingLength -= BLOCK_SIZE;	
+				fileDataOffset += BLOCK_SIZE;
+				blockNum++;
 			}
-			
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			sendPacket = new DatagramPacket(
-					sendData, all_data.length, IPAddress, port);
-			serverSocket.send(sendPacket);
-			
-			//for(int i = 0, block_count = 0; i < all_data.length; i++)
-			//{
-			//	int start_of_block = 0;
-			//	if(block_count == 0)
-			//		start_of_block = i;
-			//		
-			//	sendData[block_count] = all_data[i];
-			//	if(block_count == BLOCK_SIZE-1 || remaining_data_length < BLOCK_SIZE)
-			//	{
-			//		//send packet
-			//		InetAddress IPAddress = receivePacket.getAddress();
-			//		int port = receivePacket.getPort();
-			//		if(sendData.length == BLOCK_SIZE)
-			//			sendPacket = new DatagramPacket(
-			//				sendData, start_of_block, BLOCK_SIZE, IPAddress, port);
-			//		else
-			//			sendPacket = new DatagramPacket(
-			//				sendData, start_of_block, remaining_data_length, IPAddress, port);
-			//		serverSocket.send(sendPacket);					
-			//		remaining_data_length -= BLOCK_SIZE;
-			//		
-			//		if(remaining_data_length <= 0)
-			//			break;
-			//		
-			//		//start next packet
-			//		if(remaining_data_length < BLOCK_SIZE)
-			//			sendData = new byte[remaining_data_length];
-			//		else
-			//			sendData = new byte[BLOCK_SIZE];
-			//		block_count = 0;
-			//	}
-			//}
-			System.out.println("Completed file request.");
+			while(remainingLength > 0);
 		}
 		catch(IOException ex)
 		{
